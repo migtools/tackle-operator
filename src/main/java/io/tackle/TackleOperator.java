cf1584f8842df8e88cb075819f6c11bb65da0881
mrizzi <mrizzi@redhat.com>
2021-12-21 18:52:03 +0100

    Debug bug and deployment describe removed

--- src/main/java/io/tackle/TackleOperator.java
+++ src/main/java/io/tackle/TackleOperator.java
@@ -19,8 +19,6 @@ public class TackleOperator implements QuarkusApplication {
 
     @Override
     public int run(String... args) throws Exception {
-        System.out.println("I'm going to exit now!");
-        System.exit(-1);
         operator.start();
         Quarkus.waitForExit();
         return 0;
