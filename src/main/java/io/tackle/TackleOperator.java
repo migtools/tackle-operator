2f1e79f3edeb0691886b3f3d5389bb8f63412e9e
mrizzi <mrizzi@redhat.com>
2021-12-21 17:45:25 +0100

    Introducing a bug deliberately

--- src/main/java/io/tackle/TackleOperator.java
+++ src/main/java/io/tackle/TackleOperator.java
@@ -19,6 +19,8 @@ public class TackleOperator implements QuarkusApplication {
 
     @Override
     public int run(String... args) throws Exception {
+        System.out.println("I'm going to exit now!");
+        System.exit(-1);
         operator.start();
         Quarkus.waitForExit();
         return 0;
